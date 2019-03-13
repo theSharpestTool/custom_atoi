9e4dcaaee271a2f70aa1047def5244e8badf9bc9
theSharpestTool <41484688+theSharpestTool@users.noreply.github.com>
2019-03-13 20:00:47 +0200

    update1

--- atoi.cpp
+++ atoi.cpp
@@ -11,26 +11,16 @@ using namespace std;
 class notNumberExeption {
 };
 
-int custom_atoi(char* str)
+int custom_atoi(const char* str)
 {
-	char numbers[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 	int result = 0, counter = 0;
 	int length = strlen(str);
-	
+
 	while (*str)
 	{
-		bool intFound = false;
-		for (int i = 0; i < 10; i++)
-		{
-			if (*str == numbers[i])
-			{
-				result += i * static_cast<int>(pow(10, length - counter - 1));
-				counter++;
-				intFound = true;
-			}
-		}
-		if (!intFound)
+		if (*str < '0' || *str > '9')
 			throw notNumberExeption();
+		result += (*str - '0') * pow(10, length - counter++ - 1);
 		str++;
 	}
 	return result;
