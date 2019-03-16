3f88f5cfd5fc7f1f8974a332532b57351d4c8b87
theSharpestTool <41484688+theSharpestTool@users.noreply.github.com>
2019-03-16 18:26:25 +0200

    upd2

--- atoi.cpp
+++ atoi.cpp
@@ -13,23 +13,34 @@ class notNumberExeption {
 
 int custom_atoi(const char* str)
 {
-	int result = 0, counter = 0;
-	int length = strlen(str);
+	int result = 0;
+	bool isNegative = false;
+	if (*str == '-')
+	{
+		isNegative = true;
+		str++;
+	}
 
 	while (*str)
 	{
 		if (*str < '0' || *str > '9')
 			throw notNumberExeption();
-		result += (*str - '0') * pow(10, length - counter++ - 1);
+		result *= 10;
+		result += (*str - '0');
 		str++;
 	}
+
+	if (isNegative)
+		result *= -1;
+
 	return result;
 }
 
 
 int main()
 {
-	cout << custom_atoi("0123") << endl;
+	const char* a = "-001234";
+	cout << custom_atoi(a) << endl;
 	system("pause");
     return 0;
 }
